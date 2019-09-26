ba0f3c83e032202cf99bc8758f14d6d415a895c8
William Koller <wkoller25@gmail.com>
2019-09-26 01:06:25 -0300

    add routes

--- Modules/Home/Routes/web.php
+++ Modules/Home/Routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::prefix('home')->group(function() {
-    Route::get('/', 'HomeController@index');
+Route::prefix('admin')->group(function() {
+    Route::get('/home', ['as' => 'admin.home.list', 'uses' => 'HomeController@list']);
+    Route::get('/home/create', ['as' => 'admin.home.create', 'uses' => 'HomeController@create'])->where('id', '[0-9]+');
+    Route::get('/home/edit/{id}', ['as' => 'admin.home.edit', 'uses' => 'HomeController@edit'])->where('id', '[0-9]+');
+    Route::post('/home/{id}', ['as' => 'admin.home.update', 'uses' => 'HomeController@update'])->where('id', '[0-9]+');
+    Route::get('/home/delete/{id}', ['as' => 'admin.home.destroy', 'uses' => 'HomeController@destroy'])->where('id', '[0-9]+');
+    Route::post('/home/', ['as' => 'admin.home.doCreate', 'uses' => 'HomeController@doCreate'])->where('id', '[0-9]+');
 });
