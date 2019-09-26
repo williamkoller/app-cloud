c010ccd13666129a1ed352eef15c000e26c24069
William Koller <wkoller25@gmail.com>
2019-09-26 00:45:31 -0300

    add model Home

--- Modules/Home/Entities/Home.php
+++ Modules/Home/Entities/Home.php
@@ -6,11 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Home extends Model
 {
+    protected $table = 'home';
+
+    protected $primaryKey = 'id';
+
     protected $fillable = [
         'name',
         'title',
         'description'
     ];
+
+    protected static $rules = [
+        'default' => [
+            'name'          => 'required|max:191',
+            'title'         => 'required|max:191',
+            'description'   => 'required|max:191'
+        ],
+    ];
+
+    protected $dates = ['created_at', 'updated_at'];
+
+    protected $casts = ['id'];
+
+
 
 
 }
